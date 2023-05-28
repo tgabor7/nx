490138e7b184db89473d9ec5a047264b3055e5e8
Gábor Tarkó <tarko.gabor@lexunit.hu>
2023-05-28 12:56:18 +0200

    Center title

--- packages/app/app/page.tsx
+++ packages/app/app/page.tsx
@@ -1,5 +1,3 @@
-import styles from './page.module.scss';
-
 export default async function Index() {
   /*
    * Replace the elements below with your own.
@@ -7,7 +5,7 @@ export default async function Index() {
    * Note: The corresponding styles are in the ./index.scss file.
    */
   return (
-    <h1 className="text-3xl font-bold">
+    <h1 className="text-3xl font-bold text-center">
       Hello world!!!
     </h1>
     
