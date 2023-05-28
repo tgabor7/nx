942d5ec1e24e8662364b71b24cdbc5967d6972e7
Gábor Tarkó <tarko.gabor@lexunit.hu>
2023-05-28 12:53:01 +0200

    First commit

---
+++ packages/app/app/page.tsx
@@ -0,0 +1,15 @@
+import styles from './page.module.scss';
+
+export default async function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.scss file.
+   */
+  return (
+    <h1 className="text-3xl font-bold">
+      Hello world!!!
+    </h1>
+    
+  );
+}
